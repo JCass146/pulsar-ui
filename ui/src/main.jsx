03b83728975a712903cf97e6a360cb1f9aad601e
JCass146 <jac2440@columbia.edu>
2026-01-29 13:18:31 -0600

    render issue

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import "./styles/variables.css";
 import "./styles/tokens.css";
 import "./styles/base.css";
 import "./styles/utilities.css";
